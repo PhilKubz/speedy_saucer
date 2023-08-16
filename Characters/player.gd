5e8123156198db67edb5a31e5ea92d93c9caf4e0
PhilKubsz <philip.kubisz@gmail.com>
2023-08-16 08:47:23 -0700

    further in lesson -- apply_force

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 
 func _ready():
-	apply_impulse(Vector2(25, 0))
+	#apply_impulse(Vector2(25, 0))
+	pass
+
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
-	pass
+	apply_force(Vector2(25, 0))
